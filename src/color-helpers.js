629262ee25220a493f555101c40a8ff05fa1cf8b
mandado <jorgerobertodev@gmail.com>
2019-01-21 01:50:29 -0200

    back old code to generate paletters

--- src/color-helpers.js
+++ src/color-helpers.js
@@ -1,15 +1,16 @@
 import chroma from 'chroma-js';
 
-export const generateColorPalette = (darkenColor) => {
-  const [h] = chroma(darkenColor).hsl();
+const getLightnessColor = (color) => {
+  const [h] = chroma(color).hsl();
   
-  return [
-    chroma([h, .57, .15, 1], 'hsl').css('hsl'),
-    chroma([h, .66, .27, 1], 'hsl').css('hsl'),
-    chroma([h, .69, .38, 1], 'hsl').css('hsl'),
-    chroma([h, .64, .48, 1], 'hsl').css('hsl'),
-    chroma([h, .62, .62, 1], 'hsl').css('hsl'),
-    chroma([h, .83, .81, 1], 'hsl').css('hsl'),
-    chroma([h, 1, .97, 1], 'hsl').css('hsl'),
-  ];
+  return chroma([h, 1, .97, 1], 'hsl').css('hsl');
+};
+
+export const generateColorPalette = (darkenColor) => {
+  const lightColor = getLightnessColor(darkenColor);
+  
+  return chroma.scale([
+    lightColor, 
+    darkenColor
+  ]).mode('hsl').colors(10);
 }
